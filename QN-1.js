744be9ed7d279d5f49a3e6972161af3943dc2b53
afrahzayan <afrahzayanzaya@gmail.com>
2026-03-28 14:37:33 +0530

    first commit

---
+++ QN-1.js
@@ -0,0 +1,12 @@
+function EvenOdd(num) {
+
+    if(num % 2 === 0){
+        console.log("even");
+        
+    }else{
+       console.log("odd");
+    }
+
+    
+}EvenOdd(4)
+EvenOdd(5)
---
+++ QN-2.js
@@ -0,0 +1,7 @@
+function Count(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log(i)
+    }
+}
+
+ Count(5)
