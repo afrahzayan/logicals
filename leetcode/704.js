56a0416d5f4fae846d30ae3f6300f07d7ffa929b
afrahzayan <afrahzayanzaya@gmail.com>
2026-06-05 11:42:33 +0530

    q 704.js

---
+++ leetcode/704.js
@@ -0,0 +1,9 @@
+ function search(nums, target) {
+    if(nums.includes(target)){
+       return nums.indexOf(target)
+    }else{
+        return -1
+    }
+    
+}
+console.log(search([-1,0,3,5,9,12],9));
